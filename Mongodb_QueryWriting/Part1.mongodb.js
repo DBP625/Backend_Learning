266891896e57f8f1cb9e84c230df54ea96457080
DBP625 <pantho625@gmail.com>
2026-05-24 00:51:46 +0600

    Mongodb_Practicing

--- Mongodb_QueryWriting/Part1.mongodb.js
+++ Mongodb_QueryWriting/Part1.mongodb.js
@@ -3,13 +3,39 @@ use("mongo-practice");
 //Database Creation
 // db.createCollection("users")
 
-ds.createCollection("employees");
+//db.createCollection("employees");
 
 //Inseting One entry
-// db.users.insertOne({
-//   name: "Denesh",
-//   age: 23,
-//   identity: "Bangladeshi",
-// });
+/*
+db.users.insertOne({
+  name: "Denesh",
+  age: 23,
+  identity: "Bangladeshi",
+});
+*/
 
 //Inserting Mutiple Entry
+/*
+db.employees.insertMany([
+    {
+        name: "Jane Smith",
+        age: 30 ,
+        department: "IT",
+        salary: 70000,
+        hireDate: new Date("2019-03-10"),
+        city: "San Fransisco"
+
+    },
+    {
+        name: "Michael Jhonson",
+        age: 35 ,
+        department: "Project Manager",
+        salary: 90000,
+        hireDate: new Date("2020-04-12"),
+        city: "Chicago"
+    }
+])
+*/
+
+//Read/Find Documents
+db.employees.find();
