9276947b19c23c926904033b081ddb1ddccf6089
DBP625 <pantho625@gmail.com>
2026-05-24 12:18:55 +0600

    Mongodb Aggregation

--- Mongodb_QueryWriting/Part1.mongodb.js
+++ Mongodb_QueryWriting/Part1.mongodb.js
@@ -38,4 +38,106 @@ db.employees.insertMany([
 */
 
 //Read/Find Documents
-db.employees.find();
+// db.employees.find().pretty()
+
+//Find with Condition
+// db.employees.find({department: "IT"}).pretty()
+
+//Find with Projection
+//db.employees.find({} , {name:1 , city:1}).pretty()
+
+//Update One Document
+/*
+db.employees.updateOne(
+  { _id: ObjectId("6a11f18ab499ed80f585bed7") },
+  {
+    $set: {
+      salary: 75000,
+      city: "Dhaka",
+    },
+  },
+);
+*/
+
+// db.employees.insertMany([
+//     {
+//         name: "Rabiul Alvi",
+//         age: 34 ,
+//         department: "Finance",
+//         salary: 50000,
+//         hireDate: new Date("2034-03-10"),
+//         city: "Bogura"
+
+//     },
+//     {
+//         name: "Chanky Riyard",
+//         age: 57 ,
+//         department: "Marketing",
+//         salary: 34000,
+//         hireDate: new Date("2020-04-12"),
+//         city: "Chicago"
+//     }
+// ])
+
+//Deletion
+// db.employees.deleteOne(
+//     { _id: ObjectId('6a12036303d4a18853e9d712')}
+// )
+
+//Aggregation Operations
+
+// total Row Count
+// db.employees.aggregate([
+//     {$count: 'total'}
+// ])
+
+//Limiting Data
+// db.employees.aggregate([{ $limit: 2 }]);
+
+//Sorting Data
+
+///Sorting Salary by Descending Order
+// db.employees.aggregate([
+//     {
+//         $sort: {
+//             salary: 1
+//         }
+//     }
+// ])
+
+///Sorting Salary by Ascending Order
+// db.employees.aggregate([
+//     {
+//         $sort: {
+//             salary: -1
+//         }
+//     }
+// ])
+
+//Filtering [Match Operator]
+
+// $eq , $lt , $gt , $lte , $gte , $ne , $in , $nin, $and , $or , $not , $nor
+
+// db.employees.aggregate([
+//   {
+//     $match: {
+//       salary:{$gt:30000}
+//     }
+//   }
+// ])
+
+// db.employees.aggregate([
+//   {
+//     $match: {
+//       salary:{$eq:50000}
+//     }
+//   }
+// ])
+
+db.employees.aggregate([
+  {
+    $match: {
+      salary: { $gte: 50000 },
+    },
+  },
+]);
